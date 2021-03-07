6cd2d8af03f6fe0f49bf6293c197e6a5d1076106
Eddy <eddy.hubert@yahoo.fr>
2021-03-07 18:12:45 +0100

    fin de journée

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views # import views so we can use them in urls.
 
 
 urlpatterns = [
     url(r'^$', views.index), # "/store" will call the method "index" in "views.py"
-    path('store/', views.index), # "/store" will call the method "index" in "views.py"
+    path('products/', views.index), # "/store" will call the method "index" in "views.py"
 ]
