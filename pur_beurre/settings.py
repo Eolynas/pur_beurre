86e18c1a89601aad20070f6c0d27fcf3fe183484
Eddy <eddy.hubert53@gmail.com>
2021-04-27 14:49:30 +0200

    edit setting (db)

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -160,7 +160,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     STATICFILES_DIRS = (
         os.path.join(PROJECT_ROOT, 'static'),
     )
-    # db_from_env = dj_database_url.config(conn_max_age=500)
-    # DATABASES['default'].update(db_from_env)
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
