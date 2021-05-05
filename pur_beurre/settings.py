f7ccdadf31f034651caf6498cc53770ddec1074c
Eddy <eddy.hubert53@gmail.com>
2021-05-05 08:05:50 +0200

    add env for test in github (file yml)

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -91,21 +91,26 @@ WSGI_APPLICATION = 'pur_beurre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
-            'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
-            'NAME': 'pur_beurre', # le nom de notre base de donnees creee precedemment
-            'USER': 'postgres', # attention : remplacez par votre nom d'utilisateur
-            # 'PASSWORD': os.environ.get("db_pswd"),
-            'PASSWORD': '0311',
-            'HOST': 'localhost',
-            'PORT': '5432',
+if os.environ.get('ENV') == 'GITHUB':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
-}
+    }
+
+else:
+    DATABASES = {
+        'default': {
+                'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
+                'NAME': 'pur_beurre', # le nom de notre base de donnees creee precedemment
+                'USER': 'postgres', # attention : remplacez par votre nom d'utilisateur
+                # 'PASSWORD': os.environ.get("db_pswd"),
+                'PASSWORD': '0311',
+                'HOST': 'localhost',
+                'PORT': '5432',
+            }
+    }
 
 
 # Password validation
