733e1a575645de45cd0fa04ebf192b7ce77491cd
Eddy <eddy.hubert72@outlook.fr>
2021-07-01 12:40:16 +0200

    add fonction list_product with filter

--- product/static/product/js/scripts.js
+++ product/static/product/js/scripts.js
@@ -1,13 +1,16 @@
 /* Fixed navbar after scroll */
 
-document.addEventListener("DOMContentLoaded", function(){
-  window.addEventListener('scroll', function() {
-      if (window.scrollY > 50) {
-        document.getElementById('mainNav').classList.add('navbar_scroll');
-      }
-      else {
-        document.getElementById('mainNav').classList.remove('navbar_scroll');
-        document.body.style.paddingTop = '0';
-      }
-  });
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            document.getElementById('mainNav').classList.add('navbar_scroll');
+        } else {
+            document.getElementById('mainNav').classList.remove('navbar_scroll');
+            document.body.style.paddingTop = '0';
+        }
+    });
 });
+
+$(document).ready(function () {
+        $('#example').DataTable();
+    });
